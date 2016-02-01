69e1b4ee0a16701ee772c1571ff26fa882e370fd
Todd Wacker <twac05@yahoo.com>
2016-01-31 20:49:28 -0600

    Point to right asset file

--- config/environment.js
+++ config/environment.js
@@ -48,7 +48,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/syncano-todo';
+    ENV.baseURL = '/syncano-todo-filter';
   }
 
   return ENV;
